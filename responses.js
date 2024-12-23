ec696ba806d1a7e4bbf41f9d0c8f2975a28db1bd
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-12-23 12:25:37 -0500

    controllers (#2)

---
+++ responses.js
@@ -0,0 +1,6 @@
+const responses = {
+    hola: "¡Hola! Bienvenido a multidestinos, te habla cuchobot. Selecciona un destino turístico en Colombia escribiendo un número:\n1. Cartagena\n2. Medellín\n3. Bogotá\n4. Santa Marta",
+    default: "Lo siento, no entiendo tu mensaje. Por favor, escribe 'hola' para comenzar.",
+};
+
+export default responses;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import chatbot from './chatbot.js'
 
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req,res) => { res .json({ message: 'servidor en linea'})});
+router.use('/chatbot', chatbot)
 
 export default router
