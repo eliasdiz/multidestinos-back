b1f2c00c207b736ec0aa40a5bd0331685a65b271
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2025-01-14 21:18:56 -0500

    conexion con qr a whatsapp web (#4)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import logger from 'morgan'
 import { __dirname } from './utils.js';
 import indexRouter from './routes/index.js'
 import cors from 'cors'
+import whatsappCliente from './config/whatsapp.js';
 
 
 
@@ -16,6 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+whatsappCliente.initialize()
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
