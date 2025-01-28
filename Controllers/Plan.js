bda28bc7f2af0f247d722e800a6927fd01bd107b
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2025-01-28 12:51:20 -0500

    Elias (#21)
    * conexion con qr a whatsapp web
    
    * conexion al whatsapp ok
    
    * conexion persistente y estados de conexion, primeros mensajes automaticos
    
    * integracion con openai
    
    * conexion a remoteauth wwebjs-mongo
    
    * login, logout y remmota(mongo) autenticacion exitosa
    
    * fix
    
    * Socket: configuracion para escuchar y emitir eventos al front
    
    * Auth: controlador y ruta para verificar si la sesion de wwebjs esta guardada en la DB
    
    * Descripcion: modelo ruta y controlador
    
    * Fechas del Plan: controlador modelo y ruta. actualiza la descripcion, si no la hay la crea

--- Controllers/Plan.js
+++ Controllers/Plan.js
@@ -1,5 +1,5 @@
 import Descripcion from "../models/Descripcion.js";
-
+import Fechas from '../models/Fechas.js'
 
 const controller = {
 
@@ -12,16 +12,59 @@ const controller = {
             let descripcion = await Descripcion.findOneAndUpdate(
                 {},
                 data,
-                {new: true, upsert: true}
+                {upsert: true}
             )
             return res
                 .status(200)
                 .json({
-                    message: descripcion ? 'descripcion creada' : 'descripcion actualizada',
+                    message: descripcion ? 'descripcion actualizada' : 'descripcion creada',
                     descripcion
             })
         } catch (error) {
-            console.log(error)
+            // console.log(error)
+            next(error)
+        }
+    },
+
+    getDescripcion: async (req,res,next) => {
+        try {
+            let descripcion = await Descripcion.findOne()
+            return res
+                .status(200)
+                .json({ descripcion})
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    fechas: async (req,res,next) => {
+        let data = {
+            fechas: req.body.fechas || 'fechas'
+        }
+        try {
+            let fechas = await Fechas.findOneAndUpdate(
+            {},
+            data,
+            {upsert: true}
+            )
+            return res 
+                .status(200)
+                .json({
+                    message: fechas ? 'fechas actualizadas' : 'fechas creadas exitosamente',
+                    fechas
+                })
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    getFechas: async (req,res,next) => {
+        try {
+            let fechas = await Fechas.findOne()
+            return res
+                .status(200)
+                .json({ fechas })
+        } catch (error) {
             next(error)
         }
     }
